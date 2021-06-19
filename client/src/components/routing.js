4ba187a8ab7b02d90d62da0b4a4ea7e950beda38
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-18 22:04:55 -0500

    close

--- client/src/components/routing.js
+++ client/src/components/routing.js
@@ -6,7 +6,7 @@ export function Routing() {
     return (
         <Switch>            
             <Route path="/improved_portfolio" component={Portfolio} />
-            <Route path="/improved_portfolio" component={Pages} />
+            <Route path="/portfolio" component={Pages} />
             <Route render={() => <h1>Page not found</h1>} />
         </Switch>
     );
