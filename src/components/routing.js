3f58c7e4bc0db076fde7a0b0f9e7645d820b4508
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-18 22:54:58 -0500

    all

--- src/components/routing.js
+++ src/components/routing.js
@@ -5,7 +5,7 @@ import {Pages, Portfolio} from "../pages/pages";
 export function Routing() {
     return (
         <Switch>            
-            <Route path="/improved_portfolio/portfolio" component={Portfolio} />
+            <Route path="/portfolio" component={Portfolio} />
             <Route path="/" component={Pages} />
             <Route render={() => <h1>Page not found</h1>} />
         </Switch>
