fd73f444296f04f48873e7e00f4e69c3d60a80a5
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-19 00:13:35 -0500

    woops

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+        <div className="footer">
+        Contact Me: &nbsp;
+        <a href="mailto: wardrs438@gmail.com" style={{ fontSize: '20px', textAlign: '30px', textColor: 'white' }}>Email</a>&nbsp;&nbsp;
+        <a href="https://github.com/ward438/" style={{ fontSize: '20px', textAlign: '30px' }}>GitHub</a>
+    </div>
+    )   
+}
+
+export default Footer;
