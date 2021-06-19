c4ad9e3ca9a2208b10ffbcb330e3de5ee5233667
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-18 19:26:22 -0500

    test

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-const express = require("express");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.use(favicon(__dirname + 'client/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+app.listen(port);
