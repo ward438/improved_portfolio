88439acb0a9a759579a73660d25f6a7c14ba09b2
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-18 22:06:59 -0500

    routing issues

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -6,13 +6,13 @@ import '../index.css'
 function Header() {
   return (
     <Navbar bg="secondary" expand="lg" className='{navbar}'>
-      <Navbar.Brand style={{ color: 'white', fontSize: '25px'}}>RW </Navbar.Brand>&nbsp;&nbsp;&nbsp;
+      <Navbar.Brand style={{ color: 'white', fontSize: '25px' }}>RW </Navbar.Brand>&nbsp;&nbsp;&nbsp;
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/improved_portfolio" style={{ color: 'white', fontSize: '20px' }}>Home </Nav.Link>&nbsp;&nbsp;
-          <Nav.Link href="/" style={{ color: 'white',fontSize: '20px'}}>Portfolio</Nav.Link>          
-        </Nav>       
+          <Nav.Link href="/" style={{ color: 'white', fontSize: '20px' }}>Home </Nav.Link>&nbsp;&nbsp;
+          <Nav.Link href="/improved_portfolio" style={{ color: 'white', fontSize: '20px' }}>Portfolio</Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
--- client/src/components/routing.js
+++ client/src/components/routing.js
@@ -5,7 +5,7 @@ import {Pages, Portfolio} from "../pages/pages";
 export function Routing() {
     return (
         <Switch>            
-            <Route path="/improved_portfolio" component={Portfolio} />
+            <Route path="/" component={Portfolio} />
             <Route path="/portfolio" component={Pages} />
             <Route render={() => <h1>Page not found</h1>} />
         </Switch>
